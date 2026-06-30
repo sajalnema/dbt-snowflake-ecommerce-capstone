c8f875ac2f787c34ea75a0f59e94b2b78ffea232
Sajal Nema <nemasajal13@gmail.com>
2026-06-30 13:10:12 +0530

    feat(staging): standardize customer and order staging models

--- ecommerce_capstone/ecommerce_capstone/models/staging/stg_customers.sql
+++ ecommerce_capstone/ecommerce_capstone/models/staging/stg_customers.sql
@@ -1,8 +1,10 @@
 SELECT
     id AS customer_id,
-    TRIM(first_name) AS first_name,
-    TRIM(last_name) AS last_name,
+    INITCAP(TRIM(first_name)) AS first_name,
+    INITCAP(TRIM(last_name)) AS last_name,
     LOWER(TRIM(email)) AS email,
-    signup_date
+    CAST(signup_date AS DATE) AS signup_date
 FROM
     {{ref('raw_customers')}}
+WHERE
+    email IS NOT NULL
--- ecommerce_capstone/ecommerce_capstone/models/staging/stg_orders.sql
+++ ecommerce_capstone/ecommerce_capstone/models/staging/stg_orders.sql
@@ -1,9 +1,23 @@
 SELECT
     order_id,
     user_id AS customer_id,
-    order_timestamp
-    status,
+    TO_TIMESTAMP(
+        REGEXP_REPLACE(
+            order_timestamp,
+            '(st|nd|rd|th)',
+            ''
+        ),
+        'DD MONTH YYYY HH24:MI:SS'
+    ) AS order_timestamp,
+
+    CASE
+        WHEN status = 1 THEN 'Completed'
+        WHEN status = 2 THEN 'Returned'
+        ELSE 'Cancelled'
+    END AS status,
+
     amount
 FROM
     {{ref('raw_orders')}}
 
+WHERE user_id IS NOT NULL
