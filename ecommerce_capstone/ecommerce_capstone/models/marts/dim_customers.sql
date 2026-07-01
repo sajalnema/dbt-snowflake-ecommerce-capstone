fceabeac0a93fe8c8b7249841986fe605c644689
Sajal Nema <nemasajal13@gmail.com>
2026-07-01 12:20:27 +0530

    feature/marts- build customer dimension and orders fact models

---
+++ ecommerce_capstone/ecommerce_capstone/models/marts/dim_customers.sql
@@ -0,0 +1,16 @@
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.email,
+    c.signup_date,
+    o.first_order_date,
+    o.most_recent_order_date,
+    COALESCE(o.total_orders_placed, 0) AS total_orders_placed,
+    COALESCE(o.customer_lifetime_value, 0) AS customer_lifetime_value
+FROM 
+    {{ ref('stg_customers') }} c
+LEFT JOIN 
+    {{ ref('int_customer_order_summary') }} o
+
+ON c.customer_id = o.customer_id
