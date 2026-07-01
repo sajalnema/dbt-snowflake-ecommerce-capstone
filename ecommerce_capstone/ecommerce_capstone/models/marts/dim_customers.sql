6385d28b5b7af4fc93cacea14df05b4f1d4ecbf2
Sajal Nema <nemasajal13@gmail.com>
2026-07-01 14:11:15 +0530

    enhance customer dimension with production metadata

--- ecommerce_capstone/ecommerce_capstone/models/marts/dim_customers.sql
+++ ecommerce_capstone/ecommerce_capstone/models/marts/dim_customers.sql
@@ -1,20 +1,53 @@
-{{config(
-    materialized='incremental',
-    unique_key='customer_id',
-)}}
-SELECT
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    c.email,
-    c.signup_date,
-    o.first_order_date,
-    o.most_recent_order_date,
-    COALESCE(o.total_orders_placed, 0) AS total_orders_placed,
-    COALESCE(o.customer_lifetime_value, 0) AS customer_lifetime_value
-FROM 
-    {{ ref('stg_customers') }} c
-LEFT JOIN 
-    {{ ref('int_customer_order_summary') }} o
+{{
+    config(
+        materialized='incremental',
+        unique_key='customer_id'
+    )
+}}
 
-ON c.customer_id = o.customer_id
+WITH customer_profile AS (
+
+    SELECT
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        c.email,
+        c.signup_date,
+        o.first_order_date,
+        o.most_recent_order_date,
+        COALESCE(o.total_orders_placed, 0) AS total_orders_placed,
+        COALESCE(o.customer_lifetime_value, 0) AS customer_lifetime_value,
+        CASE
+            WHEN COALESCE(o.total_orders_placed, 0) > 0
+            THEN TRUE
+            ELSE FALSE
+        END AS active_customer,
+
+        CURRENT_TIMESTAMP() AS updated_at
+
+    FROM {{ ref('stg_customers') }} c
+
+    LEFT JOIN {{ ref('int_customer_order_summary') }} o
+        ON c.customer_id = o.customer_id
+
+)
+
+SELECT
+    *,
+    MD5(
+        CONCAT_WS(
+            '|',
+            COALESCE(customer_id::VARCHAR, ''),
+            COALESCE(first_name, ''),
+            COALESCE(last_name, ''),
+            COALESCE(email, ''),
+            COALESCE(signup_date::VARCHAR, ''),
+            COALESCE(first_order_date::VARCHAR, ''),
+            COALESCE(most_recent_order_date::VARCHAR, ''),
+            COALESCE(total_orders_placed::VARCHAR, '0'),
+            COALESCE(customer_lifetime_value::VARCHAR, '0'),
+            COALESCE(active_customer::VARCHAR, '')
+        )
+    ) AS row_hash
+
+FROM customer_profile
